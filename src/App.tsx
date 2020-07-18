703a7d5f543f8c48e37ef74960d4fe65acdcf7a8
Chase Clettenberg <clettenberg@gmail.com>
2020-07-18 18:59:59 -0500

    Fetch trips from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({trips: []});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/trips');
+      setData(result.data);
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.trips.map(trip => trip["title"])}
     </div>
   );
 }
